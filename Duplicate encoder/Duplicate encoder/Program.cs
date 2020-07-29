3edb65471c5fc4a2dd83236eab265f8374a158f2
Kirillabockiy <Kirillabockiy@gmail.com>
2020-07-29 16:05:33 +0700

    add dublicate encoder

---
+++ Duplicate encoder/Duplicate encoder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Duplicate_encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputStr = Console.ReadLine().ToLower();
+            string outputStr = inputStr;
+
+            char[] inputCharArray = inputStr.ToCharArray();
+            char[] outputCharArray = new char[inputCharArray.Length];
+
+
+           
+
+            for (int i = 0; i < inputCharArray.Length; i++)
+            {
+
+
+                int count = inputCharArray.Count(ch => ch == inputCharArray[i]);
+
+                if (count == 1)
+                {
+                    outputStr = outputStr.Replace(inputCharArray[i], '(');
+                }
+                if (count > 1)
+                {
+                    outputStr = outputStr.Replace(inputCharArray[i], ')');
+                }
+            }
+
+
+            Console.WriteLine(outputStr);
+            
+        }
+    }
+}
